7c0e31b23b448f3b5a6cca86e834abfade93991d
Александр <kozin.last@yandex.ru>
2024-05-14 21:17:12 +0300

    Исправил замечания!

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 public class Main {
     //Задача №1
-    private static void checkYearAndPrint(int numbers) {
-        if (numbers > 1584 && (numbers % 4 == 0 && numbers % 100 != 0 || numbers % 400 == 0)) {
-            System.out.printf("Год %s является высокосным", numbers);
+    private static void checkYearAndPrint(int years) {
+        if (years > 1584 && (years % 4 == 0 && years % 100 != 0 || years % 400 == 0)) {
+            System.out.printf("Год %d является высокосным", years);
         } else {
-            System.out.printf("Год %s не является высокосным", numbers);
+            System.out.printf("Год %d не является высокосным", years);
         }
 
     }
 
     //Задача №2
-    private static void getYearAndOCAndPrint(int OC, int year, int currentYear) {
-        if (OC == 0 && year < currentYear) {
+    private static void getYearAndOCAndPrint(int ocType, int currentYear, int year) {
+        if (ocType == 0 && currentYear < year) {
             System.out.println("Установите облегченную версию приложения для IOS по ссылке");
-        } else if (OC == 0 && year >= currentYear) {
+        } else if (ocType == 0 && currentYear >= year) {
             System.out.println("Установите версию приложения для IOS по ссылке");
-        } else if (OC == 1 && year < currentYear) {
+        } else if (ocType == 1 && currentYear < year) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (OC == 1 && year >= currentYear) {
+        } else if (ocType == 1 && currentYear >= year) {
             System.out.println("Установите версию приложения для Android по ссылке");
         } else {
             System.out.println("Приложение не установится");
@@ -30,14 +30,14 @@ public class Main {
         int days = 1;
         if (distance <= 20) {
             return "Потребуется дней: " + days;
-        } else if (distance > 20 && days <= 60) {
+        } else if (distance > 20 && distance <= 60) {
             return ("Потребуется дней: " + (days + 1));
-        } else if (distance > 60 && days <= 100) {
+        } else if (distance > 60 && distance <= 100) {
             return ("Потребуется дней: " + (days + 2));
         } else if (distance > 100) {
-            return ("Доставки нет");
+           return "Доставки нет";
         }
-        return null;
+        return days + "";
     }
 
 
@@ -53,7 +53,7 @@ public class Main {
         int borderYear = 2015;
         getYearAndOCAndPrint(clientDeviceOC, clientDeviceYear, borderYear);
         //Задача №3
-        int deliveryDistance = 95;
+        int deliveryDistance = 102;
         System.out.println(getDistanceDaysAndPrint(deliveryDistance));
 
 
